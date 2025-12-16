f4d8513449d845980a00f87bc3403b7a1d9be541
Michal <polonecmichal36@gmail.com>
2025-12-16 16:55:19 +0100

    Update uloha12.py

--- uloha12.py
+++ uloha12.py
@@ -1,9 +1,9 @@
-cisla = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-parne = []
-neparne = []
-for cislo in cisla:
-    if cislo % 2 == 0:
-        parne.append(cislo)
-    else:
-        neparne.append(cislo)
-print("parne su:", parne, "neparne su:", neparne)
+def p_n(z):
+    parne = []
+    neparne = []
+    for cislo in z:
+        if cislo % 2 == 0:
+            parne.append(cislo)
+        else:
+            neparne.append(cislo)
+    return("parne su:", parne, "neparne su:", neparne)
